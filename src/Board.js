486b886943e27b665a691dee7bdc341a9ec1960b
elfo404 <grdnricci@gmail.com>
2017-05-19 20:16:57 +0200

    Major refactoring

--- src/Board.js
+++ src/Board.js
@@ -3,81 +3,24 @@ import "./Board.css"
 import Square from "./Square";
 
 class Board extends Component {
-	size = 4;
-	moves=0;
-
-	constructor() {
-		super();
-		let squares = [];
-
-		let i;
-		for (i = 0; i < Math.pow(this.size, 2) - 1; i++) {
-			squares.push({
-				index: i,
-				square: <Square key={i} value={i + 1} onClick={() => this.handleClick(i)}/>
-			})
-		}
-		squares.push({ index: i, square: null });
-
-		this.state = {
-			squares: squares
-		};
-	}
-
-	handleClick(i) {
-		if (this.canMove(i)) {
-			this.moves++;
-			this.move(i);
-		}
-	}
-
-	move(i) {
-		let A = this.state.squares;
-		console.log("Swapping " + A[A.findIndex(e => e.index === i)].square.props.value);
-
-		let emptyIndex = A[A.findIndex(e => e.square === null)].index;
-
-		A[A.findIndex(e => e.square === null)].index = A[A.findIndex(e => e.index === i)].index;
-		A[A.findIndex(e => e.index === i)].index = emptyIndex;
-
-		this.setState({ squares: A });
-	}
-
-	canMove(index) {
-		let A = this.state.squares;
-		let top = A.findIndex(e => e.index + this.size === index);
-		let bottom = A.findIndex(e => e.index - this.size === index);
-		let left = A.findIndex(e => e.index + 1 === index && (index + 1) % this.size !== 1);
-		let right = A.findIndex(e => e.index - 1 === index && (index + 1) % this.size !== 0);
-
-		return (typeof A[top] !== 'undefined' && A[top].square === null)
-			|| (typeof A[bottom] !== 'undefined' && A[bottom].square === null)
-			|| ( typeof A[left] !== 'undefined' && A[left].square === null )
-			|| ( typeof A[right] !== 'undefined' && A[right].square === null  );
-	}
-
 	renderSquare(v, k) {
-		return <Square key={v} value={v} onClick={() => this.handleClick(k)} y={parseInt(k / this.size, 0)}
-		               x={k % this.size}/>
+		const size = Math.sqrt(this.props.squares.length);
+		return <Square key={v} value={v} onClick={() => this.props.onClick(k)} y={parseInt(k / size, 0)}
+		               x={k % size}/>
 	}
 
 	render() {
-		console.info("render");
 		return (
 			<div>
-				<span>Moves: {this.moves}</span>
 				<div className="board">
-					{this.state.squares.map((object, i) => {
-						if (object.square) {
-							return object ? this.renderSquare(object.square.props.value, object.index) : null
-						} else {
-							return null;
-						}
+					{this.props.squares.map((object, i) => {
+						return object.square ? this.renderSquare(object.square.props.value, object.index) : null
 					})}
 				</div>
 			</div>
 		);
 	}
 }
+
 
 export default Board;
--- src/Game.js
+++ src/Game.js
@@ -1,23 +1,73 @@
 import React, { Component } from 'react';
 import Board from "./Board";
-
-
 import './Game.css';
+import Square from "./Square";
 
-class App extends Component {
-  render() {
-    return (
-        <div className="game">
-          <div className="game-board">
-            <Board />
-          </div>
-          <div className="game-info">
-            <div>{/* status */}</div>
-            <ol>{/* TODO */}</ol>
-          </div>
-        </div>
-    );
-  }
+class Game extends Component {
+	size=4;
+	constructor() {
+		super();
+		let squares = [];
+
+		let i;
+		for (i = 0; i < Math.pow(this.size, 2) - 1; i++) {
+			squares.push({
+				index: i,
+				square: <Square key={i} value={i + 1} onClick={() => this.handleClick(i)}/>
+			})
+		}
+		squares.push({ index: i, square: null });
+
+		this.state = {
+			squares: squares
+		};
+	}
+
+	handleClick(i) {
+		if (this.canMove(i)) {
+			this.move(i);
+		}
+	}
+
+	canMove(index) {
+		let A = this.state.squares;
+		let top = A.findIndex(e => e.index + this.size === index);
+		let bottom = A.findIndex(e => e.index - this.size === index);
+		let left = A.findIndex(e => e.index + 1 === index && (index + 1) % this.size !== 1);
+		let right = A.findIndex(e => e.index - 1 === index && (index + 1) % this.size !== 0);
+
+		return (typeof A[top] !== 'undefined' && A[top].square === null)
+			|| (typeof A[bottom] !== 'undefined' && A[bottom].square === null)
+			|| ( typeof A[left] !== 'undefined' && A[left].square === null )
+			|| ( typeof A[right] !== 'undefined' && A[right].square === null  );
+	}
+
+	move(i) {
+		let A = this.state.squares;
+		console.log("Swapping " + A[A.findIndex(e => e.index === i)].square.props.value);
+
+		let emptyIndex = A[A.findIndex(e => e.square === null)].index;
+
+		A[A.findIndex(e => e.square === null)].index = A[A.findIndex(e => e.index === i)].index;
+		A[A.findIndex(e => e.index === i)].index = emptyIndex;
+
+		this.setState({ squares: A });
+	}
+
+	render() {
+		return (
+			<div className="game">
+				<div className="game-board">
+					<Board squares={this.state.squares}
+					       onClick={(i) => this.handleClick(i)}/>
+				</div>
+				<div className="game-info">
+					<div>{/* status */}</div>
+					<ol>{/* TODO */}</ol>
+				</div>
+			</div>
+		);
+	}
 }
 
-export default App;
+export default Game;
